491e64e57da3e41f127a0ae532489983a2bc958f
Connor Frendt <cjacksonfx@yahoo.com>
2018-10-30 19:24:34 -0700

    Implemented stats on page w/ bg

--- js/pokedex-filter.js
+++ js/pokedex-filter.js
@@ -4,7 +4,7 @@ const typeInput = document.getElementById('filter-type');
 const pokedexFilter = {
     init(onFilter) {
         nameInput.addEventListener('keyup', function() {
-            onFilter(nameInput.nodeValue, typeInput.value);
+            onFilter(nameInput.value, typeInput.value);
         });
         
         typeInput.addEventListener('keyup', function() {
--- js/pokedex-table.js
+++ js/pokedex-table.js
@@ -5,9 +5,15 @@ const tableBody = document.getElementById('pokedex-body');
 function makeRow(pokemon) {
     return html`<tr>
         <td style="background-color: ${pokemon.color_1}">${pokemon.pokemon}</td>
-        <td style="background-color: red">${pokemon.hp}</td>
         <td class="${pokemon.type_1}"></td>
         <td class="${pokemon.type_2}"></td>
+        <td style="background-color: red">${pokemon.hp}</td>
+        <td style="background-color: orange">${pokemon.attack}</td>
+        <td style="background-color: yellow">${pokemon.defense}</td>
+        <td style="background-color: lightblue">${pokemon.special_attack}</td>
+        <td style="background-color: lightgreen">${pokemon.special_defense}</td>
+        <td style="background-color: pink">${pokemon.speed}</td>
+        <td style="background-color: lightgray">${pokemon.generation_id}</td>
     </tr>`;
 }
 
